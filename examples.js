a67e26419274453bf013b1f45a85774d8262f841
Amylo <amylose1110@gmail.com>
2023-04-10 21:11:11 +0900

    initial commit

---
+++ examples.js
@@ -0,0 +1,16 @@
+"use strict";
+class Person {
+    hello() {
+        console.log('안녕하세요', Person.CITY);
+    }
+    change() {
+        Person.CITY = "LA";
+    }
+}
+Person.CITY = "SEOUL";
+const p1 = new Person();
+p1.hello();
+const p2 = new Person();
+p2.hello();
+p1.change();
+p2.hello();
---
+++ examples.ts
@@ -0,0 +1,17 @@
+class Person {
+    private static CITY = "SEOUL";
+    public hello() {
+        console.log('안녕하세요', Person.CITY)
+    }
+    public change() {
+        Person.CITY = "LA";
+    }
+}
+
+const p1 = new Person();
+p1.hello();
+
+const p2 = new Person();
+p2.hello();
+p1.change();
+p2.hello();
